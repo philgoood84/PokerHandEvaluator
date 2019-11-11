bef3903e62baf0213c27584d3f828c3ab55bd133
Benedikt Óskarsson <benedikto16@ru.is>
2019-11-11 04:40:33 +0000

    Improve Python performance (#16)
    * Setup the project structure
    
    * Created all hashtables for the algorithm
    
    * Finished writing the evaluator
    
    * Restructure of the code and examples
    
    * Created the numpy card, modified evaluator to use string ('Ah') fromat
    
    * Created tests with 1000 examples for each
    
    * change to requirements
    
    * Finished the code for 9 hands
    
    * Updated travis file
    
    * Update travis and readme
    
    * Updated travis file
    
    * Performance upgrade
    
    * Remove C++ test file
    
    * Deleted card class for performance, and little clean up

--- python/phevaluator/evaluator/evaluator.py
+++ python/phevaluator/evaluator/evaluator.py
@@ -1,32 +1,51 @@
-from evaluator.card import Card
 from evaluator.evaluator5 import evaluate_5cards
 from evaluator.evaluator6 import evaluate_6cards
 from evaluator.evaluator7 import evaluate_7cards
 from evaluator.evaluator8 import evaluate_8cards
 from evaluator.evaluator9 import evaluate_9cards
 
+rank_map = {
+    '2': 0,
+    '3': 1,
+    '4': 2,
+    '5': 3,
+    '6': 4,
+    '7': 5,
+    '8': 6,
+    '9': 7,
+    'T': 8,
+    'J': 9,
+    'Q': 10,
+    'K': 11,
+    'A': 12
+}
+
+suit_map = {
+    'C': 0,
+    'D': 1,
+    'H': 2,
+    'S': 3,
+    'c': 0,
+    'd': 1,
+    'h': 2,
+    's': 3
+}
+
 def evaluate_cards(*args):
+  if isinstance(args[0], str):
+    cards = []
+    for arg in args:
+      cards.append(rank_map[arg[0]] * 4 + suit_map[arg[1]])
+  else:
+    cards = tuple(args) 
+    
   if len(args) == 5:
-    return evaluate_5cards(
-      Card(args[0]), Card(args[1]), Card(args[2]), Card(args[3]), Card(args[4])
-    )
+    return evaluate_5cards(*cards)
   elif len(args) == 6:
-    return evaluate_6cards(
-      Card(args[0]), Card(args[1]), Card(args[2]),
-      Card(args[3]), Card(args[4]), Card(args[5])
-    )
+    return evaluate_6cards(*cards)
   elif len(args) == 7:
-    return evaluate_7cards(
-      Card(args[0]), Card(args[1]), Card(args[2]), Card(args[3]),
-      Card(args[4]), Card(args[5]), Card(args[6])
-    )
+    return evaluate_7cards(*cards)
   elif len(args) == 8:
-    return evaluate_8cards(
-      Card(args[0]), Card(args[1]), Card(args[2]), Card(args[3]),
-      Card(args[4]), Card(args[5]), Card(args[6]), Card(args[7])
-    )
+    return evaluate_8cards(*cards)
   elif len(args) == 9:
-    return evaluate_9cards(
-      Card(args[0]), Card(args[1]), Card(args[2]), Card(args[3]), Card(args[4]),
-      Card(args[5]), Card(args[6]), Card(args[7]), Card(args[8])
-    )
+    return evaluate_9cards(*cards)
