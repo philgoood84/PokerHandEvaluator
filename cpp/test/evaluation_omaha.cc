00c0209c5f74b81b5ce4afafb3eab2621122c58a
Henry Lee <lee0906@hotmail.com>
2020-05-22 17:46:17 +1000

    Use randome sample to test the Omaha evaluator

--- cpp/test/evaluation_omaha.cc
+++ cpp/test/evaluation_omaha.cc
@@ -1,6 +1,10 @@
 #include <cstdio>
 #include <cassert>
 #include <algorithm>
+#include <vector>
+#include <set>
+#include <chrono>
+#include <random>
 #include <phevaluator/phevaluator.h>
 #include "gtest/gtest.h"
 #include "kev/kev_eval.h"
@@ -11,86 +15,103 @@ static int percentage(long long numerator, long long denominator) {
   return numerator * 100 / denominator;
 }
 
+static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+static std::default_random_engine generator(seed);
+static std::uniform_int_distribution<int> distribution(0, 51);
+
+static std::vector<int> RandomCardSample(int size) {
+  std::vector<int> ret;
+  std::set<int> sample;
+
+  while (sample.size() < size) {
+    int number = distribution(generator);
+    if (sample.find(number) == sample.end()) {
+      ret.push_back(number);
+      sample.insert(number);
+    }
+  }
+
+  return ret;
+}
+
+static short IterateKevEval(int a, int b, int c, int d, int e,
+                            int f, int g, int h, int i) {
+  short best = 20000;
+
+  int board[10][3] = {
+    {a, b, c},
+    {a, b, d},
+    {a, b, e},
+    {a, c, d},
+    {a, c, e},
+    {a, d, e},
+    {b, c, d},
+    {b, c, e},
+    {b, d, e},
+    {c, d, e},
+  };
+  int hole[6][2] = {
+    {f, g},
+    {f, h},
+    {f, i},
+    {g, h},
+    {g, i},
+    {h, i},
+  };
+
+  for (int j = 0; j < 10; j++) {
+    for (int k = 0; k < 6; k++) {
+      best = std::min(kev_eval_5cards(board[j][0], board[j][1], board[j][2],
+            hole[k][0], hole[k][1]), best);
+    }
+  }
+
+  return best;
+}
+
 TEST(EvaluationTest, TestOmahaCards) {
-  long long count = 0;
   int progress = 0;
-  const long long total = 3679075400;
+  const long long total = 10 * 1000 * 1000;
 
   std::printf("Start testing Omaha cards\n");
 
-  for(int a = 0; a < 44; a ++)
-  {
-    for(int b = a + 1; b < 45; b++)
-    {
-      for(int c = b + 1; c < 46; c++)
-      {
-        for(int d = c + 1; d < 47; d++)
-        {
-          for(int e = d + 1; e < 48; e++)
-          {
-            for(int f = e + 1; f < 49; f++)
-            {
-              for(int g = f + 1; g < 50; g++)
-              {
-                for(int h = g + 1; h < 51; h++)
-                {
-                  for(int i = h + 1; i < 52; i++)
-                  {
-                    short best = 20000;
+  for (long long count = 0; count < total; count++) {
+    std::vector<int> sample = RandomCardSample(9);
+  
+    int ph_eval = EvaluateOmahaCards(sample[0],
+                                     sample[1],
+                                     sample[2],
+                                     sample[3],
+                                     sample[4],
+                                     sample[5],
+                                     sample[6],
+                                     sample[7],
+                                     sample[8]).value();
 
-                    int board[10][3] = {
-                      {a, b, c},
-                      {a, b, d},
-                      {a, b, e},
-                      {a, c, d},
-                      {a, c, e},
-                      {a, d, e},
-                      {b, c, d},
-                      {b, c, e},
-                      {b, d, e},
-                      {c, d, e},
-                    };
-                    int hole[6][2] = {
-                      {f, g},
-                      {f, h},
-                      {f, i},
-                      {g, h},
-                      {g, i},
-                      {h, i},
-                    };
+    int kev_eval = IterateKevEval(sample[0],
+                                  sample[1],
+                                  sample[2],
+                                  sample[3],
+                                  sample[4],
+                                  sample[5],
+                                  sample[6],
+                                  sample[7],
+                                  sample[8]);
 
-                    for (int j = 0; j < 10; j++) {
-                      for (int k = 0; k < 6; k++) {
-                        best = std::min(kev_eval_5cards(board[j][0], board[j][1], board[j][2],
-                            hole[k][0], hole[k][1]), best);
-                      }
-                    }
-                    int ph_eval = EvaluateOmahaCards(a, b, c, d, e, f, g, h, i).value();
+    EXPECT_EQ(ph_eval, kev_eval) << "Cards are: "
+      << sample[0] << ", " << sample[1] << ", " << sample[2] << ", " << sample[3] << ", "
+      << sample[4] << ", " << sample[5] << ", " << sample[6] << ", " << sample[7] << ", "
+      << sample[8] << ", ";
 
-                    EXPECT_EQ(ph_eval, best) << "Cards are: "
-                      << a << ", " << b << ", " << c << ", " << d << ", "
-                      << e << ", " << f << ", " << g << ", " << h << ", "
-                      << i << ", ";
-
-                    count++;
-
-                    if (percentage(count, total) > progress) {
-                      progress = percentage(count, total);
-                      if (progress % 2 == 0) {
-                        std::printf("Test progress: %d%%\n", progress);
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
+    if (percentage(count, total) > progress) {
+      progress = percentage(count, total);
+      if (progress % 10 == 0) {
+        std::printf("Test progress: %d%%\n", progress);
       }
     }
   }
 
   std::printf("Complete testing Omaha cards\n");
-  std::printf("Tested %lld hands in total\n", count);
+  std::printf("Tested %lld random hands in total\n", total);
 }
 
