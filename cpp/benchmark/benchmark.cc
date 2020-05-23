0250637af5a771bb3f1adb5ebfd5ab7e88172353
Henry Lee <lee0906@hotmail.com>
2020-05-23 22:43:16 +1000

    Add random samples in the benchmark

--- cpp/benchmark/benchmark.cc
+++ cpp/benchmark/benchmark.cc
@@ -1,5 +1,8 @@
-#include <cstdio>
-#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <chrono>
+#include <random>
 #include "phevaluator/phevaluator.h"
 #include "benchmark/benchmark.h"
 
@@ -62,30 +65,91 @@ static void EvaluateAllSevenCards(benchmark::State& state) {
 }
 BENCHMARK(EvaluateAllSevenCards);
 
-static void EvaluateAllOmahaCards(benchmark::State& state) {
-  for (auto _ : state) {
-    for(int a = 0; a < 44; a++) {
-      for(int b = a + 1; b < 45; b++) {
-        for(int c = b + 1; c < 46; c++) {
-          for(int d = c + 1; d < 47; d++) {
-            for(int e = d + 1; e < 48; e++) {
-              for(int f = e + 1; f < 49; f++) {
-                for(int g = f + 1; g < 50; g++) {
-                  for(int h = g + 1; h < 51; h++) {
-                    for(int i = h + 1; i < 52; i++) {
-                      evaluate_omaha_cards(a, b, c, d, e, f, g, h, i);
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+static std::default_random_engine generator(seed);
+static std::uniform_int_distribution<int> distribution(0, 51);
+
+static std::vector<int> RandomCardSample(int size) {
+  std::vector<int> ret;
+  std::set<int> sample;
+
+  while (sample.size() < size) {
+    int number = distribution(generator);
+    if (sample.find(number) == sample.end()) {
+      ret.push_back(number);
+      sample.insert(number);
     }
   }
+
+  return ret;
 }
-BENCHMARK(EvaluateAllOmahaCards);
+
+static void EvaluateRandomFiveCards(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    std::vector<int> randomCards = RandomCardSample(5);
+    state.ResumeTiming();
+
+    EvaluateCards(randomCards[0],
+                  randomCards[1],
+                  randomCards[2],
+                  randomCards[3],
+                  randomCards[4]);
+  }
+}
+BENCHMARK(EvaluateRandomFiveCards);
+
+static void EvaluateRandomSixCards(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    std::vector<int> randomCards = RandomCardSample(6);
+    state.ResumeTiming();
+
+    EvaluateCards(randomCards[0],
+                  randomCards[1],
+                  randomCards[2],
+                  randomCards[3],
+                  randomCards[4],
+                  randomCards[5]);
+  }
+}
+BENCHMARK(EvaluateRandomSixCards);
+
+static void EvaluateRandomSevenCards(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    std::vector<int> randomCards = RandomCardSample(7);
+    state.ResumeTiming();
+
+    EvaluateCards(randomCards[0],
+                  randomCards[1],
+                  randomCards[2],
+                  randomCards[3],
+                  randomCards[4],
+                  randomCards[5],
+                  randomCards[6]);
+  }
+}
+BENCHMARK(EvaluateRandomSevenCards);
+
+static void EvaluateRandomOmahaCards(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    std::vector<int> randomCards = RandomCardSample(9);
+    state.ResumeTiming();
+
+    EvaluateOmahaCards(randomCards[0],
+                       randomCards[1],
+                       randomCards[2],
+                       randomCards[3],
+                       randomCards[4],
+                       randomCards[5],
+                       randomCards[6],
+                       randomCards[7],
+                       randomCards[8]);
+  }
+}
+BENCHMARK(EvaluateRandomOmahaCards);
 
 BENCHMARK_MAIN();
 
